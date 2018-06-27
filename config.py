3a95ab5e3cc61470e6c35df29a10bf4469a7b8d6
SarahYuHanCheng <bigbigtreeful@gmail.com>
2018-06-27 17:56:59 +0800

    Ch9 Pagination

--- config.py
+++ config.py
@@ -15,4 +15,6 @@ class Config(object):
 	MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
 	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 	ADMINS = ['sarah12110211@yahoo.com.tw']
+	POSTS_PER_PAGE = 3
+	
 
