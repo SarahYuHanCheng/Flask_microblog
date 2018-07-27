325499ca7233333c5825627a29e015f4aa8d037a
SarahYuHanCheng <bigbigtreeful@gmail.com>
2018-07-27 19:18:21 +0800

    add websocket server

--- app/__init__.py
+++ app/__init__.py
@@ -8,8 +8,7 @@ import logging #python package, DEBUG, INFO, WARNING, ERROR and CRITICAL
 from logging.handlers import SMTPHandler, RotatingFileHandler
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
-
-
+# from flask_socketio import SocketIO
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -27,6 +26,9 @@ def create_app(config_class=Config):
 	app = Flask(__name__)
 	app.config.from_object(config_class)
 
+	# socketio = SocketIO(app)
+	
+
 	db.init_app(app)
 	migrate.init_app(app,db)
 	login.init_app(app)
@@ -34,7 +36,7 @@ def create_app(config_class=Config):
 	bootstrap.init_app(app)
     # moment.init_app(app)
     # babel.init_app(app)
-
+	
 	from app.errors import bp as errors_bp
 	app.register_blueprint(errors_bp)
 
@@ -85,5 +87,5 @@ def create_app(config_class=Config):
 
 		app.logger.addHandler(logging.INFO)
 		app.logger.info('Microblog startup')
-
+		# socketio.run(app, debug=True,async_mode='threading')
 	return app
