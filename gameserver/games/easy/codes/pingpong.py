4465c17b4e8cd487e6f809073621b4a195170e91
SarahYuHanCheng <bigbigtreeful@gmail.com>
2018-08-04 20:08:52 +0800

    worker with queue

--- gameserver/games/easy/codes/pingpong.py
+++ gameserver/games/easy/codes/pingpong.py
@@ -1,193 +1,41 @@
-# Developer : Hamdy Abou El Anein
+import multiprocessing, queue, time
 
-import random
-import pygame
-import sys
-from pygame import *
+class Worker(multiprocessing.Process):
 
-from socketIO_client import SocketIO, LoggingNamespace
+    def __init__(self, work_queue):
+        # base class initialization
+        multiprocessing.Process.__init__(self)
 
-pygame.init()
-fps = pygame.time.Clock()
-cnt =0
-WHITE = (255, 255, 255)
-ORANGE = (255,140,0)
-GREEN = (0, 255, 0)
-BLACK = (0, 0, 0)
+        # job management stuff
+        self.work_queue = work_queue
+        self.kill_received = False
 
+    def run(self):
 
-WIDTH = 600
-HEIGHT = 400
-BALL_RADIUS = 20
-PAD_WIDTH = 8
-PAD_HEIGHT = 80
-HALF_PAD_WIDTH = PAD_WIDTH // 2
-HALF_PAD_HEIGHT = PAD_HEIGHT // 2
-ball_pos = [0, 0]
-ball_vel = [0, 0]
-paddle1_vel = 0
-paddle2_vel = 0
-l_score = 0
-r_score = 0
+        while not self.kill_received:
+
+            # get a task
+            try:
+                n_job, data = self.work_queue.get_nowait()
+            except Queue.Empty:
+                break
+
+            # the actual processing
+            print("Starting %d ..." % n_job)
+            print(str(data))
+            time.sleep(10)
 
 
-window = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)
-pygame.display.set_caption('Daylight Pong')
+if __name__ == "__main__":
 
-# from websocket import create_connection
-# ws = create_connection("ws://localhost:5000")
+    data1 = 'bob'
+    data2 = 'joe'
 
-# print("Receiving...")
-# result =  ws.recv()
-# print("Received '%s'" % result)
+    work_queue = multiprocessing.Queue()
+    for n, data in enumerate([data1, data2]):
+        work_queue.put( (n, data) )
 
-def ball_init(right):
-    global ball_pos, ball_vel
-    ball_pos = [WIDTH // 2, HEIGHT // 2]
-    horz = random.randrange(2, 4)
-    vert = random.randrange(1, 3)
-
-    if right == False:
-        horz = - horz
-
-    ball_vel = [horz, -vert]
-
-
-def init():
-    global paddle1_pos, paddle2_pos, paddle1_vel, paddle2_vel, l_score, r_score  # these are floats
-    global score1, score2  # these are ints
-    paddle1_pos = [HALF_PAD_WIDTH - 1, HEIGHT // 2]
-    paddle2_pos = [WIDTH + 1 - HALF_PAD_WIDTH, HEIGHT //2]
-    l_score = 0
-    r_score = 0
-    if random.randrange(0, 2) == 0:
-        ball_init(True)
-    else:
-        ball_init(False)
-
-def log():
-    room=sys.argv[1]
-    #save data websocket client
-    with SocketIO('localhost', 5000, LoggingNamespace) as socketIO:
-        socketIO.emit('connectfromgame',{'msg':tuple([ball_pos,room,paddle1_pos,paddle2_pos])})
-
-def draw(canvas):
-    global paddle1_pos, paddle2_pos, ball_pos, ball_vel, l_score, r_score, cnt
-    
-    canvas.fill(BLACK)
-    pygame.draw.line(canvas, WHITE, [WIDTH // 2, 0], [WIDTH // 2, HEIGHT], 1)
-    pygame.draw.line(canvas, WHITE, [PAD_WIDTH, 0], [PAD_WIDTH, HEIGHT], 1)
-    pygame.draw.line(canvas, WHITE, [WIDTH - PAD_WIDTH, 0], [WIDTH - PAD_WIDTH, HEIGHT], 1)
-    pygame.draw.circle(canvas, WHITE, [WIDTH // 2, HEIGHT // 2], 70, 1)
-
-
-    if paddle1_pos[1] > HALF_PAD_HEIGHT and paddle1_pos[1] < HEIGHT - HALF_PAD_HEIGHT:
-        paddle1_pos[1] += paddle1_vel
-    elif paddle1_pos[1] == HALF_PAD_HEIGHT and paddle1_vel > 0:
-        paddle1_pos[1] += paddle1_vel
-    elif paddle1_pos[1] == HEIGHT - HALF_PAD_HEIGHT and paddle1_vel < 0:
-        paddle1_pos[1] += paddle1_vel
-
-    if paddle2_pos[1] > HALF_PAD_HEIGHT and paddle2_pos[1] < HEIGHT - HALF_PAD_HEIGHT:
-        paddle2_pos[1] += paddle2_vel
-    elif paddle2_pos[1] == HALF_PAD_HEIGHT and paddle2_vel > 0:
-        paddle2_pos[1] += paddle2_vel
-    elif paddle2_pos[1] == HEIGHT - HALF_PAD_HEIGHT and paddle2_vel < 0:
-        paddle2_pos[1] += paddle2_vel
-
-    
-    ball_pos[0] += int(ball_vel[0])
-    ball_pos[1] += int(ball_vel[1])
-    cnt+=1
-    if cnt>5:
-        cnt=0
-        log()
-
-    pygame.draw.circle(canvas, ORANGE, ball_pos, 20, 0)
-    pygame.draw.polygon(canvas, GREEN, [[paddle1_pos[0] - HALF_PAD_WIDTH, paddle1_pos[1] - HALF_PAD_HEIGHT],
-                                        [paddle1_pos[0] - HALF_PAD_WIDTH, paddle1_pos[1] + HALF_PAD_HEIGHT],
-                                        [paddle1_pos[0] + HALF_PAD_WIDTH, paddle1_pos[1] + HALF_PAD_HEIGHT],
-                                        [paddle1_pos[0] + HALF_PAD_WIDTH, paddle1_pos[1] - HALF_PAD_HEIGHT]], 0)
-    pygame.draw.polygon(canvas, GREEN, [[paddle2_pos[0] - HALF_PAD_WIDTH, paddle2_pos[1] - HALF_PAD_HEIGHT],
-                                        [paddle2_pos[0] - HALF_PAD_WIDTH, paddle2_pos[1] + HALF_PAD_HEIGHT],
-                                        [paddle2_pos[0] + HALF_PAD_WIDTH, paddle2_pos[1] + HALF_PAD_HEIGHT],
-                                        [paddle2_pos[0] + HALF_PAD_WIDTH, paddle2_pos[1] - HALF_PAD_HEIGHT]], 0)
-
-
-    if int(ball_pos[1]) <= BALL_RADIUS:
-        ball_vel[1] = - ball_vel[1]
-    if int(ball_pos[1]) >= HEIGHT + 1 - BALL_RADIUS:
-        ball_vel[1] = -ball_vel[1]
-
-
-    if int(ball_pos[0]) <= BALL_RADIUS + PAD_WIDTH and int(ball_pos[1]) in range(paddle1_pos[1] - HALF_PAD_HEIGHT,
-                                                                                 paddle1_pos[1] + HALF_PAD_HEIGHT, 1):
-        ball_vel[0] = -ball_vel[0]
-        ball_vel[0] *= 1.1
-        ball_vel[1] *= 1.1
-    elif int(ball_pos[0]) <= BALL_RADIUS + PAD_WIDTH:
-        r_score += 1
-        ball_init(True)
-
-    if int(ball_pos[0]) >= WIDTH + 1 - BALL_RADIUS - PAD_WIDTH and int(ball_pos[1]) in range(
-            paddle2_pos[1] - HALF_PAD_HEIGHT, paddle2_pos[1] + HALF_PAD_HEIGHT, 1):
-        ball_vel[0] = -ball_vel[0]
-        ball_vel[0] *= 1.1
-        ball_vel[1] *= 1.1
-    elif int(ball_pos[0]) >= WIDTH + 1 - BALL_RADIUS - PAD_WIDTH:
-        l_score += 1
-        ball_init(False)
-
-
-    myfont1 = pygame.font.SysFont("Comic Sans MS", 20)
-    label1 = myfont1.render("Score " + str(l_score), 1, (255, 255, 0))
-    canvas.blit(label1, (50, 20))
-
-    myfont2 = pygame.font.SysFont("Comic Sans MS", 20)
-    label2 = myfont2.render("Score " + str(r_score), 1, (255, 255, 0))
-    canvas.blit(label2, (470, 20))
-
-
-def keydown(event):
-    global paddle1_vel, paddle2_vel
-
-    if event.key == K_UP:
-        paddle2_vel = -8
-    elif event.key == K_DOWN:
-        paddle2_vel = 8
-    elif event.key == K_z:
-        paddle1_vel = -8
-    elif event.key == K_s:
-        paddle1_vel = 8
-
-
-def keyup(event):
-    global paddle1_vel, paddle2_vel
-
-    if event.key in (K_z, K_s):
-        paddle1_vel = 0
-    elif event.key in (K_UP, K_DOWN):
-        paddle2_vel = 0
-
-
-init()
-
-
-while True:
-
-    draw(window)
-    
-
-    for event in pygame.event.get():
-
-        if event.type == KEYDOWN:
-            keydown(event)
-        elif event.type == KEYUP:
-            keyup(event)
-        elif event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-            ws.close()
-
-    pygame.display.update()
-    fps.tick(30)
+    num_processes = 2
+    for i in range(num_processes):
+        worker = Worker(work_queue)
+        worker.start()
