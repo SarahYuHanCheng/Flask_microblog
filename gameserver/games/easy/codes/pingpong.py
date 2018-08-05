39dbdc449bccb0b6a1d086b8a625990a3d7b9634
SarahYuHanCheng <bigbigtreeful@gmail.com>
2018-08-05 20:18:30 +0800

    no pygame

--- gameserver/games/easy/codes/pingpong.py
+++ gameserver/games/easy/codes/pingpong.py
@@ -1,41 +1,144 @@
-import multiprocessing, queue, time
-
-class Worker(multiprocessing.Process):
-
-    def __init__(self, work_queue):
-        # base class initialization
-        multiprocessing.Process.__init__(self)
-
-        # job management stuff
-        self.work_queue = work_queue
-        self.kill_received = False
-
-    def run(self):
-
-        while not self.kill_received:
-
-            # get a task
-            try:
-                n_job, data = self.work_queue.get_nowait()
-            except Queue.Empty:
-                break
-
-            # the actual processing
-            print("Starting %d ..." % n_job)
-            print(str(data))
-            time.sleep(10)
+import random
+import time
+import sys
+# from p76051080 import p1
+# p1()
+from socketIO_client import SocketIO, LoggingNamespace
 
 
-if __name__ == "__main__":
+cnt =0
+WHITE = (255, 255, 255)
+ORANGE = (255,140,0)
+GREEN = (0, 255, 0)
+BLACK = (0, 0, 0)
 
-    data1 = 'bob'
-    data2 = 'joe'
 
-    work_queue = multiprocessing.Queue()
-    for n, data in enumerate([data1, data2]):
-        work_queue.put( (n, data) )
+WIDTH = 600
+HEIGHT = 400
+BALL_RADIUS = 20
+PAD_WIDTH = 8
+PAD_HEIGHT = 80
+HALF_PAD_WIDTH = PAD_WIDTH // 2
+HALF_PAD_HEIGHT = PAD_HEIGHT // 2
+ball_pos = [0, 0]
+ball_vel = [0, 0]
+paddle1_vel = 0
+paddle2_vel = 0
+l_score = 0
+r_score = 0
 
-    num_processes = 2
-    for i in range(num_processes):
-        worker = Worker(work_queue)
-        worker.start()
+def ball_init(right):
+    global ball_pos, ball_vel
+    ball_pos = [WIDTH // 2, HEIGHT // 2]
+    horz = random.randrange(2, 4)
+    vert = random.randrange(1, 3)
+
+    if right == False:
+        horz = - horz
+
+    ball_vel = [horz, -vert]
+
+
+def init():
+    global paddle1_pos, paddle2_pos, paddle1_vel, paddle2_vel, l_score, r_score  # these are floats
+    global score1, score2  # these are ints
+    paddle1_pos = [HALF_PAD_WIDTH - 1, HEIGHT // 2]
+    paddle2_pos = [WIDTH + 1 - HALF_PAD_WIDTH, HEIGHT //2]
+    l_score = 0
+    r_score = 0
+    if random.randrange(0, 2) == 0:
+        ball_init(True)
+    else:
+        ball_init(False)
+
+def log():
+    room=sys.argv[1]
+    print(ball_pos)
+    #save data websocket client
+    with SocketIO('localhost', 5000, LoggingNamespace) as socketIO:
+        socketIO.emit('connectfromgame',{'msg':tuple([ball_pos,room,paddle1_pos,paddle2_pos])})
+
+def draw():
+    global paddle1_pos, paddle2_pos, ball_pos, ball_vel, l_score, r_score, cnt
+
+
+    if paddle1_pos[1] > HALF_PAD_HEIGHT and paddle1_pos[1] < HEIGHT - HALF_PAD_HEIGHT:
+        paddle1_pos[1] += paddle1_vel
+    elif paddle1_pos[1] == HALF_PAD_HEIGHT and paddle1_vel > 0:
+        paddle1_pos[1] += paddle1_vel
+    elif paddle1_pos[1] == HEIGHT - HALF_PAD_HEIGHT and paddle1_vel < 0:
+        paddle1_pos[1] += paddle1_vel
+
+    if paddle2_pos[1] > HALF_PAD_HEIGHT and paddle2_pos[1] < HEIGHT - HALF_PAD_HEIGHT:
+        paddle2_pos[1] += paddle2_vel
+    elif paddle2_pos[1] == HALF_PAD_HEIGHT and paddle2_vel > 0:
+        paddle2_pos[1] += paddle2_vel
+    elif paddle2_pos[1] == HEIGHT - HALF_PAD_HEIGHT and paddle2_vel < 0:
+        paddle2_pos[1] += paddle2_vel
+
+
+    ball_pos[0] += int(ball_vel[0])
+    ball_pos[1] += int(ball_vel[1])
+    cnt+=1
+    if cnt>5:
+        cnt=0
+        log()
+
+
+    if int(ball_pos[1]) <= BALL_RADIUS:
+        ball_vel[1] = - ball_vel[1]
+    if int(ball_pos[1]) >= HEIGHT + 1 - BALL_RADIUS:
+        ball_vel[1] = -ball_vel[1]
+
+
+    if int(ball_pos[0]) <= BALL_RADIUS + PAD_WIDTH and int(ball_pos[1]) in range(paddle1_pos[1] - HALF_PAD_HEIGHT,
+                                                                                 paddle1_pos[1] + HALF_PAD_HEIGHT, 1):
+        ball_vel[0] = -ball_vel[0]
+        ball_vel[0] *= 1.1
+        ball_vel[1] *= 1.1
+    elif int(ball_pos[0]) <= BALL_RADIUS + PAD_WIDTH:
+        r_score += 1
+        if r_score > 2:
+            sys.exit()
+        ball_init(True)
+
+    if int(ball_pos[0]) >= WIDTH + 1 - BALL_RADIUS - PAD_WIDTH and int(ball_pos[1]) in range(
+            paddle2_pos[1] - HALF_PAD_HEIGHT, paddle2_pos[1] + HALF_PAD_HEIGHT, 1):
+        ball_vel[0] = -ball_vel[0]
+        ball_vel[0] *= 1.1
+        ball_vel[1] *= 1.1
+    elif int(ball_pos[0]) >= WIDTH + 1 - BALL_RADIUS - PAD_WIDTH:
+        l_score += 1
+        if l_score > 2:
+            sys.exit()
+        ball_init(False)
+
+
+def keydown(event):
+    global paddle1_vel, paddle2_vel
+
+    if event.key == K_UP:
+        paddle2_vel = -8
+    elif event.key == K_DOWN:
+        paddle2_vel = 8
+    elif event.key == K_z:
+        paddle1_vel = -8
+    elif event.key == K_s:
+        paddle1_vel = 8
+
+
+def keyup(event):
+    global paddle1_vel, paddle2_vel
+
+    if event.key in (K_z, K_s):
+        paddle1_vel = 0
+    elif event.key in (K_UP, K_DOWN):
+        paddle2_vel = 0
+
+
+init()
+
+
+while True:
+    draw()
+    time.sleep(0.01)
