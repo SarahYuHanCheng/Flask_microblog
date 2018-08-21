0ced3576cb03edfdc5078e199f48e70dcac5f2d0
Yu-hanCheng <sarahcheng1231@gmail.com>
2018-08-21 16:10:34 +0800

    player crtl paddle_vel not pos, add sys.exit but failed when 2 players

--- gameserver/games/easy/codes/gamemain.py
+++ gameserver/games/easy/codes/gamemain.py
@@ -3,9 +3,10 @@
 # socketio server communicate to P1,P2
 # socketio client communicate to webserver
 # -----------------------------------------
+import random
 import socketio
 import eventlet
-import pingpong
+import sys
 from flask import Flask, render_template
 
 sio = socketio.Server()
@@ -13,47 +14,140 @@ app = Flask(__name__)
 
 from socketIO_client import SocketIO, LoggingNamespace
 
-ball=[5,6]
-paddle1=[3]
-paddle2=[8]
+WIDTH = 600
+HEIGHT = 400
+BALL_RADIUS = 20
+PAD_WIDTH = 8
+PAD_HEIGHT = 80
+HALF_PAD_WIDTH = PAD_WIDTH // 2
+HALF_PAD_HEIGHT = PAD_HEIGHT // 2
+ball = [0, 0]
+ball_vel = [0, 0]
+paddle1_vel = 0
+paddle2_vel = 0
+l_score = 0
+r_score = 0
 
 @sio.on('connect')
 def connect(sid, environ):
     print("connect ", sid)
 
 @sio.on('P1')
-def print_message(sid, message):
-    print("P1 Socket ID: " , sid)
-    game(message)
+def on_P1(sid, p1_vel):
+    global paddle1_vel
+    # print("P1 " , p1_vel)
+    paddle1_vel=p1_vel
+    game()
 
 @sio.on('P2')
-def print_message(sid, message):
-    print("P2 Socket ID: " , sid)
-    game(message)
+def on_P2(sid, p2_vel):
+    global paddle2_vel
+    paddle2_vel=p2_vel
+    game()
 
 @sio.on('disconnect')#, namespace='/chat'
 def disconnect(sid):
     print('disconnect ', sid)
 
+def ball_init(right):
+    global ball, ball_vel
+    ball = [WIDTH // 2, HEIGHT // 2]
+    horz = random.randrange(2, 4)
+    vert = random.randrange(1, 3)
+
+    if right == False:
+        print("init move left")
+        horz = - horz
+
+    ball_vel = [horz, -vert]
 def __init__():
-    pass
+    global paddle1, paddle2, paddle1_vel, paddle2_vel, l_score, r_score  # these are floats
+    global score1, score2  # these are ints
+    paddle1 = [HALF_PAD_WIDTH - 1, HEIGHT // 2]
+    paddle2 = [WIDTH + 1 - HALF_PAD_WIDTH, HEIGHT //2]
+    l_score = 0
+    r_score = 0
+    if random.randrange(0, 2) == 0:
+        ball_init(True)
+    else:
+        ball_init(False)
 
 def score():
     pass
 
-def game(result):
-    print("call game:",result)
+def game():
+    play()
     send_to_Players()
 
 def send_to_Players():
-    sio.emit('gameinfo',{'msg':tuple([ball,paddle1,paddle2])})
+    sio.emit('gameinfo',{'msg':tuple([ball,paddle1[1],paddle2[1]])})
 
 def send_to_webserver():
     with SocketIO('localhost', 5000, LoggingNamespace) as socketIO:
-        socketIO.emit('connectfromgame',{'msg':tuple([ball_pos,room,paddle1_pos,paddle2_pos])})
+        socketIO.emit('connectfromgame',{'msg':tuple([ball,room,paddle1,paddle2])})
 
+def end_game():
+    print("end")
+    score()
+    sys.exit()
+
+def play():
+    global paddle1, paddle2,paddle1_vel,paddle2_vel, ball, ball_vel, l_score, r_score, cnt
+
+    print('paddle[1,2]:(%d,%d)'%(paddle1[1],paddle2[1]))
+    if paddle1[1] > HALF_PAD_HEIGHT and paddle1[1] < HEIGHT - HALF_PAD_HEIGHT:
+        paddle1[1] += paddle1_vel
+    elif paddle1[1] == HALF_PAD_HEIGHT and paddle1_vel > 0:
+        paddle1[1] += paddle1_vel
+    elif paddle1[1] == HEIGHT - HALF_PAD_HEIGHT and paddle1_vel < 0:
+        paddle1[1] += paddle1_vel
+
+    if paddle2[1] > HALF_PAD_HEIGHT and paddle2[1] < HEIGHT - HALF_PAD_HEIGHT:
+        paddle2[1] += paddle2_vel
+    elif paddle2[1] == HALF_PAD_HEIGHT and paddle2_vel > 0:
+        paddle2[1] += paddle2_vel
+    elif paddle2[1] == HEIGHT - HALF_PAD_HEIGHT and paddle2_vel < 0:
+        paddle2[1] += paddle2_vel
+
+
+    ball[0] += int(ball_vel[0])
+    ball[1] += int(ball_vel[1])
+
+
+
+    if int(ball[1]) <= BALL_RADIUS:
+        ball_vel[1] = - ball_vel[1]
+    if int(ball[1]) >= HEIGHT + 1 - BALL_RADIUS:
+        ball_vel[1] = - ball_vel[1]
+
+
+    if int(ball[0]) <= BALL_RADIUS + PAD_WIDTH and int(ball[1]) in range(paddle1[1] - HALF_PAD_HEIGHT,
+                                                                                 paddle1[1] + HALF_PAD_HEIGHT, 1):
+        ball_vel[0] = -ball_vel[0]
+        ball_vel[0] *= 1.1
+        ball_vel[1] *= 1.1
+    elif int(ball[0]) <= BALL_RADIUS + PAD_WIDTH:
+        r_score += 1
+        print('r_score ',r_score)
+        if r_score > 1:
+            end_game()
+        ball_init(True)
+
+    if int(ball[0]) >= WIDTH + 1 - BALL_RADIUS - PAD_WIDTH and int(ball[1]) in range(
+            paddle2[1] - HALF_PAD_HEIGHT, paddle2[1] + HALF_PAD_HEIGHT, 1):
+        ball_vel[0] = -ball_vel[0]
+        ball_vel[0] *= 1.1
+        ball_vel[1] *= 1.1
+    elif int(ball[0]) >= WIDTH + 1 - BALL_RADIUS - PAD_WIDTH:
+        l_score += 1
+        print('l_score ',l_score)
+        if l_score > 1:
+            end_game()
+        ball_init(False)
 
 
 if __name__ == '__main__':
-     app = socketio.Middleware(sio, app)
-     eventlet.wsgi.server(eventlet.listen(('', 8000)), app)
+    __init__()
+    app = socketio.Middleware(sio, app)
+    eventlet.wsgi.server(eventlet.listen(('', 8000)), app)
+     
--- gameserver/games/easy/codes/p76051080.py
+++ gameserver/games/easy/codes/p76051080.py
@@ -1,46 +1,48 @@
 # -----------------------------------------
 # Usercode - P1, the left one
-# input: tuple([ball,paddle1,paddle1]) 
+# input: tuple([ball,paddle1,paddle2]) 
 # 	ball_pos[x,y], paddle1_pos[y]
-# output: paddle[y]
+# output: paddle_vel[move_unit]
 # -----------------------------------------
 import time
 from socketIO_client import SocketIO, LoggingNamespace
 ball_pos=[[0,0]]
 paddle1_pos=[0] # paddle only Y axis
-move_unit=3
-
+move_unit=5
+paddle_vel=0
 
 def on_gameinfo(message):
 	print("on_gameinfo")
 	tuple_msg=message['msg']
 	ball_pos.append(tuple_msg[0])
+	paddle1_pos.append(tuple_msg[1])
 	print(ball_pos)
 	run()
 	communicate()
 
 
 def run():
+	global paddle_vel
 	if (ball_pos[-1][0]-ball_pos[-2][0]) <0: 
 		print("ball moves left")
 		if (ball_pos[-1][1]-ball_pos[-2][1]) >0:
 			print("ball moves down")
-			paddle1_pos.append(paddle1_pos[-1]+move_unit)
-
+			paddle_vel=move_unit
 		elif (ball_pos[-1][1]-ball_pos[-2][1])<0:
 			print("ball moves up")
-			paddle1_pos.append(paddle1_pos[-1]-move_unit)
+			paddle_vel=-move_unit
 	else: 
-		print("ball moves right, no need to move paddle2")
+		print("ball moves right, no need to move paddle1")
 
 def communicate():
-	time.sleep(0.1)
-	socketIO.emit('P1',paddle1_pos[-1])
+	global paddle_vel
+	time.sleep(0.01)
+	socketIO.emit('P1',paddle_vel)
 
 def score():# CPU, MEM Utility
 	pass
 
 socketIO=SocketIO('localhost', 8000, LoggingNamespace)
 socketIO.on('gameinfo',on_gameinfo)
-socketIO.emit('P1',paddle1_pos[-1])
-socketIO.wait(seconds=8)
+socketIO.emit('P1',paddle_vel)
+socketIO.wait(seconds=60)
