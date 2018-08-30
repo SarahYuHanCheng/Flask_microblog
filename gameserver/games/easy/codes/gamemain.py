2e396f3bd7544691cce1046a460ea81efe3ae548
Yu-hanCheng <sarahcheng1231@gmail.com>
2018-08-30 14:51:52 +0800

    delay of P_rt almost a sec

--- gameserver/games/easy/codes/gamemain.py
+++ gameserver/games/easy/codes/gamemain.py
@@ -229,7 +229,9 @@ def serve_app(_sio, _app):
     serversock=eventlet.wsgi.server(eventlet.listen(('', 8000)), app) 
 
 def action() :
-    print('action ! -> time : {:.1f}s'.format(time.time()-StartTime))
+    while True:
+        time.sleep(0.1)
+        print('action ! -> time : {:.1f}s'.format(time.time()-StartTime))
 
 if __name__ == '__main__':
     __init__()
@@ -238,28 +240,29 @@ if __name__ == '__main__':
     wst.start()
     StartTime=time.time()
     
+timeout= threading.Thread(target=action)
+timeout.start()
+# class setInterval :
+#     def __init__(self,interval,action) :
+#         self.interval=interval
+#         self.action=action
+#         self.stopEvent=threading.Event()
+#         thread=threading.Thread(target=self.__setInterval)
+#         thread.start()
 
-class setInterval :
-    def __init__(self,interval,action) :
-        self.interval=interval
-        self.action=action
-        self.stopEvent=threading.Event()
-        thread=threading.Thread(target=self.__setInterval)
-        thread.start()
+#     def __setInterval(self) :
+#         nextTime=time.time()+self.interval
+#         while not self.stopEvent.wait(nextTime-time.time()) :
+#             nextTime+=self.interval
+#             self.action()
 
-    def __setInterval(self) :
-        nextTime=time.time()+self.interval
-        while not self.stopEvent.wait(nextTime-time.time()) :
-            nextTime+=self.interval
-            self.action()
+#     def cancel(self) :
+#         self.stopEvent.set()
 
-    def cancel(self) :
-        self.stopEvent.set()
+# # start action every 0.6s
+# inter=setInterval(0.02,action)
+# print('just after setInterval -> time : {:.1f}s'.format(time.time()-StartTime))
 
-# start action every 0.6s
-inter=setInterval(0.02,action)
-print('just after setInterval -> time : {:.1f}s'.format(time.time()-StartTime))
-
-# will stop interval in 5s
-t=threading.Timer(25,inter.cancel)
-t.start()
+# # will stop interval in 5s
+# t=threading.Timer(25,inter.cancel)
+# t.start()
