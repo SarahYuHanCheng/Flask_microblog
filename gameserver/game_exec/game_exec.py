9d098d938edb26e62f6d9c762377b6e93ce98059
Yu-hanCheng <sarahcheng1231@gmail.com>
2018-11-12 22:22:01 +0800

    game_exec polling check serv_list then notify gameserver

--- gameserver/game_exec/game_exec.py
+++ gameserver/game_exec/game_exec.py
@@ -6,21 +6,31 @@ import json, sys,os
 import subprocess
 
 ws = create_connection("ws://localhost:6005")
-recv_msg=[]
-# def aws_container(room_name,userId,compiler,path_, filename):
-# 	# 用 subprocess將欲執行的檔名當參數,執行 exec_script.sh, 使產生 docker container 來執行程式碼,指令如下 
-# 	# sh test.sh cce238a618539(imageID) python3.7 output.py 
-# 	from subprocess import Popen, PIPE
-# 	image='cce238a618539'
-# 	try:
-# 		# room_name,userId,path,filename,'player_list'
-# 		p = Popen('sh exec_script.sh '+image+' '+compiler+' '+path_+' '+filename+'',shell=True, stdout=PIPE, stderr=PIPE)
-# 		stdout, stderr = p.communicate()
-# 		print('stdout: ',stdout)
-# 		return 0
-# 	except Exception as e:
-# 		print('e: ',e)
-# 		return 1
+serv_status=[] * 5
+
+def aws_container(log_id,userId,compiler,path_, filename):
+	# 用 subprocess將欲執行的檔名當參數,執行 exec_script.sh, 使產生 docker container 來執行程式碼,指令如下 
+	# sh test.sh cce238a618539(imageID) python3.7 output.py 
+	from subprocess import Popen, PIPE
+	try:
+		userId = Popen(''+compiler+' '+path_+' '+filename+'',shell=True)
+		return 0
+	except Exception as e:
+		print('e: ',e)
+		return 1
+
+def msg_handler(msg):
+	# 每個 element的 內容：[data['log_id'],data['user_id'],\
+	#	data['game_lib_id'],language_res[0],path,filename,data['player_list']]
+	# 開 subprocess 
+	for element in msg:
+		code = (open(""+element[4]+element[5]))
+		print(code)
+		# 執行package
+		merge_com_lib(code,element[4],element[5],element[3])
+		aws_container(element[0],element[1],element[3],element[4],element[5])
+
+
 
 def merge_com_lib(code,path,filename,compiler):
 	# "w"上傳新的程式碼會直接取代掉
@@ -52,11 +62,25 @@ def merge_com_lib(code,path,filename,compiler):
 	f.close()
 	return 0
 
-print("Sending 'Hello, World'...")
-ws.send(json.dumps({'from':"game_exec"}))
-recv_msg = ws.recv()
-merge_com_lib(recv_msg['code'], recv_msg['path'],recv_msg['filename'],recv_msg['compiler'])
-# aws_container(recv_msg['room_name'],recv_msg['userId'],recv_msg['game_id'],recv_msg['compiler'],recv_msg['code'])
+# print("Sending 'Hello, World'...")
+# ws.send(json.dumps({'from':"game_exec"}))
+# recv_msg = ws.recv()
+# msg_handler(recv_msg)
+# merge_com_lib(recv_msg['code'], recv_msg['path'],recv_msg['filename'],recv_msg['compiler'])
 
 print("Received '%s'" % recv_msg)
 
+import sched, time
+s = sched.scheduler(time.time, time.sleep)
+    
+def check_serv_status(sc):
+	global serv_status
+	lst = [i for i,x in enumerate(serv_status) if x==0]
+	if len(lst) > 0:
+		ws.send(json.dumps({'from':"game_exec"}))
+		recv_msg = ws.recv()
+		msg_handler(recv_msg)
+	s.enter(60, 1, check_serv_status, (sc,))
+
+s.enter(60, 1, check_serv_status, (s,))
+s.run()
