c3c9c0e9019c15f67206d1312433f0a4a1ddc8a3
Yu-hanCheng <sarahcheng1231@gmail.com>
2018-11-14 22:51:36 +0800

    Revert "save_code return path, filename, compiler, sort function"
    This reverts commit d007bcb028f7c455bfc2ecf134a550cb6a2aa915.

--- gameserver/gameserver.py
+++ gameserver/gameserver.py
@@ -47,16 +47,36 @@ class MaxSizeList(object):
 
 room_list=MaxSizeList(100)
 serv_list=MaxSizeList(50)
-element = [1,1,1,'python3','1/1/1/','1_1.py',['2']]
-room_list.push(element)
-element = [1,2,1,'python3','1/1/1/','1_2.py',['1']]
-room_list.push(element)
 
 def new_client(client, server):
 	msg1="Hey all, a new client has joined us"
 	# server.send_message(client,msg1)
 
+def save_code(code,room_name,user_id,game_lib_id,language,path):
+	# 在呼叫 sandbox前 將程式碼依遊戲人數？分類？為路徑 加上 lib後 存於 gameserver並回傳檔名
+	# "w"上傳新的程式碼會直接取代掉
+	filename="%s_%s%s"%(room_name,user_id,language)
+	try:
+		os.makedirs( path )
+	except:
+		pass
+	with open("%s%s"%(path,filename), "w") as f:
+		f.write(code+'\n')
+		# with open('%s%s%s'%(path,game_lib_id,language)) as fin: # lib應該是改取資料庫, 而非開文件
+		# 	lines = fin.readlines() 
+		# 	for i, line in enumerate(lines):
+		# 		if i >= 0 and i < 6800:
+		# 			f.write(line)
+	return filename
 
+def set_language(language):
+	compiler = {
+		"c": ["gcc",".c"],
+		"python": ["python3.7",".py"],
+		"shell": ["sh",".sh"]
+	}
+	language_obj = compiler.get(language, "Invalid month")
+	return language_obj 
 
 def push_to_room_list(user_code_str):
 	# 將經過 sandbox的 code 放進 room_list, check是否到齊 若有到齊, return logid, 否則 return 0
@@ -99,80 +119,30 @@ def push_to_serv_list(elephant):
 
 
 
-def sandbox(compiler,path_, filename):
-	# 用 subprocess將預測試的檔名當參數執行 script.sh, 使產生 docker container 來驗證程式碼,指令如下 
-	# sh test.sh cce238a618539(imageID) python3.7 output.py 
-	
-	from subprocess import Popen, PIPE
-	image='cce238a618539'
-	try:
-		p = Popen('sh sandbox/script.sh ' + image + ' ' + compiler + ' ' + path_ + ' '+ filename + '',shell=True, stdout=PIPE, stderr=PIPE)
-		stdout, stderr = p.communicate()
-		print('stdout:', stdout)
-		if stderr:
-			print('stderr:', stderr)
-			return [0,stderr]
-		else:
-			print('stdout:', stdout)
-			return [1,stdout]
-	except Exception as e:
-		print('e: ',e)
-		return e
-
-def save_code(code,log_id,user_id,category_id,game_id,language):
-	# data['code'],data['log_id'],data['user_id'],data['category_id'],data['game_id'],data['language']
-	# 在呼叫 sandbox前 將程式碼依遊戲人數？分類？為路徑 加上 lib後 存於 gameserver並回傳檔名
-	# "w"上傳新的程式碼會直接取代掉
-	language_res = set_language(language)
-	path = "%s/%s/%s/"%(category_id,game_id,language)
-	filename = "%s_%s%s"%(log_id,user_id,language_res[1])
-	try:
-		os.makedirs( path )
-	except:
-		pass
-	with open("%s%s"%(path,filename), "w") as f:
-		f.write(code+'\n')
-		f.write("run(3,[[1,3],[1,3],[1,3]],3)")#要給假值
-		# with open('%s%s%s'%(path,game_lib_id,language)) as fin: # lib應該是改取資料庫, 而非開文件
-		# 	lines = fin.readlines() 
-		# 	for i, line in enumerate(lines):
-		# 		if i >= 0 and i < 6800:
-		# 			f.write(line)
-	return path,filename,language_res[0]
-
-def set_language(language):
-	compiler = {
-		"c": ["gcc",".c"],
-		"python": ["python3.7",".py"],
-		"shell": ["sh",".sh"]
-	}
-	language_obj = compiler.get(language, "Invalid month")
-	return language_obj 
-
 def code_address(server,data):
 	# 先經過 sandbox, 將結果回傳給user, (確定要使用)再排進 room_list
 	global webserver_id,room_list
-	
-	path, filename, compiler = save_code(data['code'],data['log_id'],data['user_id'],data['category_id'],data['game_id'],data['language'])
+	language_res = set_language(data['language'])
+	filename=save_code(data['code'],data['room_name'],data['user_id'],data['game_lib_id'],language_res[1],path)
 	# filename include .xxx
-	test_result = sandbox(compiler,path,filename)
-	# if test_result[0]: # 1: ok / 0: error 
-	# 	msg=test_result[1]
+	test_result = sandbox(language_res[0],path,filename)
+	if test_result[0]: # 1: ok / 0: error 
+		msg=test_result[1]
 		
-	# 	log_id_index = push_to_room_list([data['log_id'],data['user_id'],\
-	# 	data['game_lib_id'],language_res[0],path,filename,data['player_list']]) # player_list must put on last
-	# 	if log_id_index > 0: # arrived 
-	# 		popped_codes_list = pop_code_in_room(log_id_index, data['log_id'])
-	# 		push_to_serv_list(popped_codes_list)
-	# 	else:
-	# 		msg +=b"wait for other players..." 
-	# 		print("wait for other players...")
-	# 		return
-	# else:
-	# 	msg =b"sandbox error output: ", test_result[1]
-	# 	print("sandbox error output: ", test_result[1])	
+		log_id_index = push_to_room_list([data['log_id'],data['user_id'],\
+		data['game_lib_id'],language_res[0],path,filename,data['player_list']]) # player_list must put on last
+		if log_id_index > 0: # arrived 
+			popped_codes_list = pop_code_in_room(log_id_index, data['log_id'])
+			push_to_serv_list(popped_codes_list)
+		else:
+			msg +=b"wait for other players..." 
+			print("wait for other players...")
+			return
+	else:
+		msg =b"sandbox error output: ", test_result[1]
+		print("sandbox error output: ", test_result[1])	
 
-	# server.send_message(webserver_id,msg.decode('utf-8')) # 回傳程式碼處理結果給user
+	server.send_message(webserver_id,msg.decode('utf-8')) # 回傳程式碼處理結果給user
 
 def message_received(client, server, message):
 	# ws server的client 來源有2: 1. webserver 2. gamemain
@@ -199,6 +169,27 @@ def message_received(client, server, message):
 	elif data['from']=='game':
 		print("gameover")
 					
+
+def sandbox(compiler,path_, filename):
+	# 用 subprocess將預測試的檔名當參數執行 script.sh, 使產生 docker container 來驗證程式碼,指令如下 
+	# sh test.sh cce238a618539(imageID) python3.7 output.py 
+	
+	from subprocess import Popen, PIPE
+	image='cce238a618539'
+	try:
+		p = Popen('sh sandbox/script.sh ' + image + ' ' + compiler + ' ' + path_ + ' '+ filename + '',shell=True, stdout=PIPE, stderr=PIPE)
+		stdout, stderr = p.communicate()
+		print('stdout:', stdout)
+		if stderr:
+			print('stderr:', stderr)
+			return [0,stderr]
+		else:
+			print('stdout:', stdout)
+			return [1,stdout]
+	except Exception as e:
+		print('e: ',e)
+		return e
+	
 	
 server.set_fn_new_client(new_client)# set callback function
 server.set_fn_message_received(message_received)
